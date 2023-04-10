23a0bbc3df07f0c49770de7fb3ed540ad30b79e1
EspiraMarvin <espiraMarvin@gmail.com>
2023-04-10 18:15:14 +0300

    feat: adds prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  // deletes bookmarks before a user, to prevent deleting a user without deleting the user's bookmark
+  cleanDb() {
+    return this.$transaction([this.bookmark.deleteMany()]);
+  }
+}
