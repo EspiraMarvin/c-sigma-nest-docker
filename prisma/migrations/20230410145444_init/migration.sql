0c371d3e0d97730c77fb82b5e340cbc87db5bcc9
EspiraMarvin <espiraMarvin@gmail.com>
2023-04-10 18:16:09 +0300

    feat: adds prisma

---
+++ prisma/migrations/20230410145444_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "bookmarks" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "link" TEXT NOT NULL,
+
+    CONSTRAINT "bookmarks_pkey" PRIMARY KEY ("id")
+);
